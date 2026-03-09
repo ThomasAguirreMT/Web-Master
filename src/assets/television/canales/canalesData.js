625ff9986dd09be799ff32a984bb5065cd910571
ThomasAguirre <tomasesteban20010@gmail.com>
2026-03-09 15:23:58 -0500

    se implementan imagenes del carrusel falta responsive

--- src/assets/television/canales/canalesData.js
+++ src/assets/television/canales/canalesData.js
@@ -11,6 +11,100 @@ import espn from "./DEPORTES/espn.png";
 import espn2 from "./DEPORTES/espn2.png";
 import espn3 from "./DEPORTES/espn3.png";
 import espn4 from "./DEPORTES/espn4.png";
+import canal_espn5 from "./DEPORTES/CANAL_ESPN5.png";
+import canal_espn6 from "./DEPORTES/CANAL_ESPN6.png";
+import canal_espn7 from "./DEPORTES/CANAL_ESPN7.png";
+import canal_win_sports from "./DEPORTES/CANAL_WIN_SPORT.png";
+import canal_azteca_deportes from "./DEPORTES/CANAL_AZTECA_DEPORTES.png";
+import canal_esports_max from "./DEPORTES/CANAL_ESPORTS_MAX.png";
+import canal_nuestra_tele from "./DEPORTES/CANAL_NUESTRA_TELE.png";
+import canal_tyc_sports from "./DEPORTES/CANAL_TYC_SPORT.png";
+
+
+
+//culturales
+import canalanimal from "./CULTURALES/canal_animal_planet.png";
+import canal_atals from "./CULTURALES/canal_atals.png";
+import canal_discovery_science from "./CULTURALES/canal_discovery_science.png";
+import canal_discovery_turbo from "./CULTURALES/canal_discovery_turbo.png";
+
+import canal_Discovery from "./CULTURALES/canal_Discovery.png";
+import canal_hgtv from "./CULTURALES/canal_hgtv.png";
+import canal_history from "./CULTURALES/canal_history.png";
+import canal_history2 from "./CULTURALES/canal_history2.png";
+import canal_ID_discovery from "./CULTURALES/canal_ID_discovery.png";
+import canal_national_geographic from "./CULTURALES/canal_national_geographic.png";
+import canal_tlc from "./CULTURALES/canal_tlc.png";
+
+
+
+// infantiles
+import CANAL_ADULT_SWIM from "./INFANTILES/CANAL_ADULT_SWIM.png";
+import CANAL_CARTOON_NETWORK from "./INFANTILES/CANAL_CARTOON_NETWORK.png";
+import CANAL_ANIMOTION from "./INFANTILES/CANAL_ANIMOTION.png";
+import CANAL_CARTOONITO from "./INFANTILES/CANAL_CARTOONITO.png";
+import CANAL_DISCOVERY_KIDS from "./INFANTILES/CANAL_DISCOVERY_KIDS.png";
+import CANAL_DISNEY from "./INFANTILES/CANAL_DISNEY_CHANNEL.png";
+import CANAL_DYSNEY_JUNIOR from "./INFANTILES/CANAL_DISNEY_JUNIOR.png";
+import CANAL_INFANTIL from "./INFANTILES/CANAL_INFANTIL.png";
+import CANAL_NICK_JUNIOR from "./INFANTILES/CANAL_NICK_JUNIOR.png";
+import   CANAL_NICKELONDEON from "./INFANTILES/CANAL_NICKELONDEON.png";
+
+
+// Peliculas
+import CANAL_CINE_CANAL  from "./PELICULAS/CANAL_CINE_CANAL.png";
+import CANAL_CINE_FAMILIAR from "./PELICULAS/CANAL_CINE_FAMILIAR.png";
+import CANAL_CINE_HISPANO from "./PELICULAS/CANAL_CINE_HISPANO.png";
+import CANAL_CINE_LATINO from "./PELICULAS/CANAL_CINE_LATINO.png";
+import CANAL_CINE_MEXICANO from "./PELICULAS/CANAL_CINE_MEXICANO.png";
+import CANAL_CINE_PREMIUM from "./PELICULAS/CANAL_CINE_PREMIUM.png";
+import CANAL_CINEMAX from "./PELICULAS/CANAL_CINEMAX.png";
+import CANAL_D_PELICULA from "./PELICULAS/CANAL_D_PELICULA.png";
+import CANAL_DREAMWORKS from "./PELICULAS/CANAL_DREAMWORKS.png";
+import CANAL_EDGE from "./PELICULAS/CANAL_EDGE.png";
+import CANAL_FMH_FAMILY from "./PELICULAS/CANAL_FMH_FAMILY.png";
+import CANAL_FMH_MOVIES from "./PELICULAS/CANAL_FMH_MOVIES.png";
+import CANAL_FX from "./PELICULAS/CANAL_FX.png";
+import CANAL_GOLDEN  from "./PELICULAS/CANAL_GOLDEN.png";
+import CANAL_MEMORIAS_FILM from "./PELICULAS/CANAL_MEMORIAS_FILM.png";
+import CANAL_SONY_MOVIES from "./PELICULAS/CANAL_SONY_MOVIES.png";
+import CANAL_SPACE from "./PELICULAS/CANAL_SPACE.png";
+import CANAL_STAR from "./PELICULAS/CANAL_STAR.png";
+import CANAL_STUDIO_UNIVERSAL from "./PELICULAS/CANAL_STUDIO_UNIVERSAL.png";
+import CANAL_TNT from "./PELICULAS/CANAL_TNT.png";
+import CANAL_UNIVERSAL from "./PELICULAS/CANAL_UNIVERSAL.png";
+
+
+//series
+import canales_AE from "./SERIES/canales_AE.png";
+import canales_AXN from "./SERIES/canales_AXN.png";
+import canales_clic from "./SERIES/canales_CLIC.png";
+import canales_comedy_central from "./SERIES/canales_COMEDY_CENTRAL.png";
+import canales_DHE from "./SERIES/canales_DHE.png";
+import canales_DISTRITO_COMEDIA from "./SERIES/canales_DISTRITO_COMEDIA.png";
+import canales_EURO_CHANNEL from "./SERIES/canales_EURO_CHANNEL.png";
+import canales_MEMORIAS_SERIES from "./SERIES/canales_MEMORIAS_SERIES.png";
+import canales_MULTI_PREMIERE from "./SERIES/canales_MULTI_PREMIERE.png";
+import canales_SONY_CHANNEL from "./SERIES/canales_SONY_CHANNEL.png";
+import canales_TCM from "./SERIES/canales_TCM.png";
+import canales_TELEMUNDO from "./SERIES/canales_TELEMUNDO.png";
+import canales_TLC from "./SERIES/canales_TLC.png";
+import canales_UNIVERSAL from "./SERIES/canales_UNIVERSAL.png";
+import canales_USA from "./SERIES/canales_USA.png";
+import canales_VE_PLUS from "./SERIES/canales_VE_PLUS.png";
+import canales_WARNET_BRETHERS from "./SERIES/canales_WARNER_BROTHERS.png";
+
+
+
+
+
+
+
+
+
+
+
+
 
 export const nacionales = [
   { nombre: "Canal Uno", canal: "Canal 1", logo: canalUno },
@@ -22,19 +116,96 @@ export const nacionales = [
 ];
 
 export const deportes = [
-  { nombre: "ESPN", canal: "Canal 14", logo: espn },
+  { nombre: "ESPN", canal: "Canal 14", logo:  espn },
   { nombre: "ESPN 2", canal: "Canal 15", logo: espn2 },
   { nombre: "ESPN 3", canal: "Canal 16", logo: espn3 },
-  { nombre: "ESPN 4", canal: "Canal 17", logo: espn4 }
+  { nombre: "ESPN 4", canal: "Canal 17", logo: espn4 },
+  { nombre: "ESPN 5", canal: "Canal 18", logo: canal_espn5 },
+  { nombre: "ESPN 6", canal: "Canal 19", logo: canal_espn6 },
+  { nombre: "ESPN 7", canal: "Canal 20", logo: canal_espn7 },
+  { nombre: "WINSPORTS", canal: "Canal 20", logo: canal_win_sports },
+  { nombre: "AZTECA DEPORTES", canal: "Canal 21", logo: canal_azteca_deportes },
+  { nombre: "ESPORTS MAX", canal: "Canal 22", logo: canal_esports_max },
+  { nombre: "NUESTRA TELE", canal: "Canal 23", logo: canal_nuestra_tele },
+  { nombre: "TYC SPORTS", canal: "Canal 24", logo: canal_tyc_sports }
+
+
+
+
 ];
 
 export const culturales = [
-  { nombre: "ESPN", canal: "Canal 14", logo: espn },
-
+  { nombre: "ANIMALPLANET", canal: "Canal 18", logo: canalanimal },
+  { nombre: "ATALS", canal: "Canal 19", logo: canal_atals },
+  { nombre: "DISCOVERY SCIENCE", canal: "Canal 20", logo: canal_discovery_science },
+  { nombre: "DISCOVERY TURBO", canal: "Canal 21", logo: canal_discovery_turbo },
+  { nombre: "DISCOVERY", canal: "Canal 22", logo: canal_Discovery },
+  { nombre: "HGTVM", canal: "Canal 23", logo: canal_hgtv },
+  { nombre: "HISTORY", canal: "Canal 24", logo: canal_history },
+  { nombre: "HISTORY 2", canal: "Canal 25", logo: canal_history2 },
+  { nombre: "ID DISCOVERY", canal: "Canal 26", logo: canal_ID_discovery },
+  { nombre: "NATIONAL GEOGRAPHIC", canal: "Canal 27", logo: canal_national_geographic },
+  { nombre: "TLC", canal: "Canal 28", logo: canal_tlc }
 ];
-export const infantiles = [];
-export const peliculas = [];
-export const series = [];
+export const infantiles = [
+  { nombre: "ADULT SWIM", canal: "Canal 29", logo: CANAL_ADULT_SWIM },
+  { nombre: "CARTOON NETWORK", canal: "Canal 30", logo: CANAL_CARTOON_NETWORK },
+  { nombre: "ANIMOTION", canal: "Canal 31", logo: CANAL_ANIMOTION },
+  { nombre: "CARTOONITO", canal: "Canal 32", logo: CANAL_CARTOONITO },
+  { nombre: "DISCOVERY KIDS", canal: "Canal 33", logo: CANAL_DISCOVERY_KIDS },
+  { nombre: "DISNEY CHANNEL", canal: "Canal 34", logo: CANAL_DISNEY },
+  { nombre: "DISNEY JUNIOR", canal: "Canal 35", logo: CANAL_DYSNEY_JUNIOR },
+  { nombre: "INFANTIL", canal: "Canal 36", logo: CANAL_INFANTIL },
+  { nombre: "NICK JUNIOR", canal: "Canal 37", logo: CANAL_NICK_JUNIOR },
+  { nombre: "NICKELONDEON", canal: "Canal 38", logo:   CANAL_NICKELONDEON }
+];
+export const peliculas = [
+  { nombre: "CINE CANAL", canal: "Canal 39", logo: CANAL_CINE_CANAL },
+  { nombre: "CINE FAMILIAR", canal: "Canal 40", logo: CANAL_CINE_FAMILIAR },
+  { nombre: "CINE HISPANO", canal: "Canal 41", logo: CANAL_CINE_HISPANO },
+  { nombre: "CINE LATINO", canal: "Canal 42", logo: CANAL_CINE_LATINO },
+  { nombre: "CINE MEXICANO", canal: "Canal 43", logo: CANAL_CINE_MEXICANO },
+  { nombre: "CINE PREMIUM", canal: "Canal 44", logo: CANAL_CINE_PREMIUM },
+  { nombre: "CINEMAX", canal: "Canal 45", logo: CANAL_CINEMAX },
+  { nombre: "D PELICULA", canal: "Canal 46", logo: CANAL_D_PELICULA },
+  { nombre: "DREAMWORKS", canal: "Canal 47", logo: CANAL_DREAMWORKS },
+  { nombre: "EDGE", canal: "Canal 48", logo: CANAL_EDGE },
+  { nombre: "FMH FAMILY", canal: "Canal 49", logo: CANAL_FMH_FAMILY },
+  { nombre: "FMH MOVIES", canal: "Canal 50", logo: CANAL_FMH_MOVIES },
+  { nombre: "FX", canal: "Canal 51", logo: CANAL_FX },
+  { nombre: "GOLDEN", canal: "Canal 52", logo: CANAL_GOLDEN },
+  { nombre: "MEMORIAS FILM", canal: "Canal 53", logo: CANAL_MEMORIAS_FILM },
+  { nombre: "SONY MOVIES", canal: "Canal 54", logo: CANAL_SONY_MOVIES },
+  { nombre: "SPACE", canal: "Canal 55", logo: CANAL_SPACE },
+  { nombre: "STAR", canal: "Canal 56", logo: CANAL_STAR },
+  { nombre: "STUDIO UNIVERSAL", canal: "Canal 57", logo: CANAL_STUDIO_UNIVERSAL },
+  { nombre: "TNT", canal: "Canal 58", logo: CANAL_TNT },
+  { nombre: "UNIVERSAL", canal: "Canal 59", logo: CANAL_UNIVERSAL }
+
+  
+];
+export const series = [
+  { nombre: "A&E", canal: "Canal 60", logo: canales_AE },
+  { nombre: "AXN", canal: "Canal 61", logo: canales_AXN },
+  { nombre: "CLIC", canal: "Canal 62", logo: canales_clic },      
+  { nombre: "COMEDY CENTRAL", canal: "Canal 63", logo: canales_comedy_central },
+  { nombre: "DHE", canal: "Canal 64", logo: canales_DHE },
+  { nombre: "DISTRITO COMEDIA", canal: "Canal 65", logo: canales_DISTRITO_COMEDIA },
+  { nombre: "EURO CHANNEL", canal: "Canal 66", logo: canales_EURO_CHANNEL },
+  { nombre: "MEMORIAS SERIES", canal: "Canal 67", logo: canales_MEMORIAS_SERIES },
+  { nombre: "MULTI PREMIERE", canal: "Canal 68", logo: canales_MULTI_PREMIERE },
+  { nombre: "SONY CHANNEL", canal: "Canal 69", logo: canales_SONY_CHANNEL },
+  { nombre: "TCM", canal: "Canal 70", logo: canales_TCM },
+  { nombre: "TELEMUNDO", canal: "Canal 71", logo: canales_TELEMUNDO },
+  { nombre: "TELEMUNDO", canal: "Canal 72", logo: canales_TELEMUNDO },
+  { nombre: "TLC", canal: "Canal 73", logo: canales_TLC },
+  { nombre: "UNIVERSAL", canal: "Canal 74", logo: canales_UNIVERSAL },
+  { nombre: "USA", canal: "Canal 75", logo: canales_USA },
+  { nombre: "VE PLUS", canal: "Canal 76", logo: canales_VE_PLUS },
+  { nombre: "WARNET BRETHERS", canal: "Canal 77", logo: canales_WARNET_BRETHERS }
+];
+
+
 export const variedades = [];
 export const musicales = [];
 export const noticias = [];
